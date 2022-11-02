a670bd543e14822cd4de9cb47fd0d828b60fde66
m-rejdych <rejdych.m.r@gmail.com>
2022-11-02 01:52:43 +0100

    Added auth middleware and protected procedure.

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,7 +1,13 @@
-import { inferAsyncReturnType } from '@trpc/server';
+import type { inferAsyncReturnType } from '@trpc/server';
+import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
 
 import { prisma } from './prisma';
+import { getSession } from '../utils/session';
 
-export const createContext = () => ({ prisma });
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const session = await getSession(opts);
+
+  return { prisma, userId: session?.user.id };
+};
 
 export type Context = inferAsyncReturnType<typeof createContext>;
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,8 +1,17 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 
 import type { Context } from './context';
 
 const t = initTRPC.context<Context>().create();
 
+const authMiddleware = t.middleware(({ ctx, next }) => {
+  if (!ctx.userId) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  return next({ ctx: { ...ctx, userId: ctx.userId } });
+});
+
 export const router = t.router;
 export const procedure = t.procedure;
+export const protectedProcedure = t.procedure.use(authMiddleware);
